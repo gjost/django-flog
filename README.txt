4c0d17012c47fc52dedf6fce926382974b32a7e6
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-07-03 11:37:02 -0700

    Moved to Sourcehut

--- README.txt
+++ README.txt
@@ -1,3 +1,1 @@
-flog - a food log
-
-Flog is a Django app for recording food consumption, weight, etc.
+Repository moved to https://git.sr.ht/~gjost/django-flog
--- flog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('flog.views',
-    (r'^(?P<yyyy>\d{4})/(?P<mm>\d{1,2})/(?P<dd>\d{1,2})/$', 'day'),
-    (r'^$', 'index'),
-)
